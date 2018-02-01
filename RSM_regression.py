113a6244e076243d4c868700a89121905681a3fe
Oolay <alex.angell@my.jcu.edu.au>
2018-02-01 16:33:35 +1100

    mixture designs added and now only evaluates one type of model at once
    Valid model types are first order ('FO'), second order ('SO'), first order and interactions ('INT') and mixture design ('MIX' - can currently only handle three independent variables)

--- RSM_regression.py
+++ RSM_regression.py
@@ -13,39 +13,43 @@ class RSM_regression(object):
         self.codedUnits = codedUnits
         self.centroids = centroids
 
-    def variable_engineer(self,X):
+    def variable_engineer(self,X, modelType = 'SO'):
 
         self.N,self.D = X.shape
+        self.modelType = modelType
 
         df_X = pd.DataFrame(X, columns = ['X' + str(i) for i in range(self.D)])
-        self.df_X_first_order = df_X.copy(deep = True)
-        self.df_X_first_order['ones'] = 1
-        self.X_first_order = self.df_X_first_order.values
 
-        self.df_X_second_order = df_X.copy(deep = True)
-        col_combs_all = itertools.combinations_with_replacement(self.df_X_second_order.columns,2)
-        for col_comb in col_combs_all:
-            self.df_X_second_order[str(col_comb[0]) + str(col_comb[1])] = self.df_X_second_order[col_comb[0]]*self.df_X_second_order[col_comb[1]]
-        self.df_X_second_order['ones'] = 1
-        self.X_second_order = self.df_X_second_order.values
+        if self.modelType == 'FO':
+            self.df_X = df_X.copy(deep = True)
+            self.df_X['ones'] = 1
 
-        self.df_X_inter = df_X.copy(deep = True)
-        col_combs_inter = itertools.combinations(self.df_X_inter.columns,2)
-        for col_comb in col_combs_inter:
-            self.df_X_inter[str(col_comb[0]) + str(col_comb[1])] = self.df_X_inter[col_comb[0]]*self.df_X_inter[col_comb[1]]
-        self.df_X_inter['ones'] = 1
-        self.X_inter = self.df_X_inter.values
+        elif self.modelType == 'SO':
+            self.df_X = df_X.copy(deep = True)
+            col_combs_all = itertools.combinations_with_replacement(self.df_X.columns,2)
+            for col_comb in col_combs_all:
+                self.df_X[str(col_comb[0]) + str(col_comb[1])] = self.df_X[col_comb[0]]*self.df_X[col_comb[1]]
+            self.df_X['ones'] = 1
 
-        '''self.df_X_mixture = df_X.copy(deep = True)
-        col_combs_mixture = itertools.combinations(self.df_X_mixture.columns,2)
-        for col_comb in col_combs_mixture:
-            self.df_X_mixture[str(col_comb[0]) + str(col_comb[1])] = self.df_X_mixture[col_comb[0]]*self.df_X_mixture[col_comb[1]]
-        self.df_X_mixture['X0X1X2'] = self.df_X_mixture['X0']*self.df_X_mixture['X1']*self.df_X_mixture['X2']'''
+        elif self.modelType == 'INT':
+            self.df_X = df_X.copy(deep = True)
+            col_combs = itertools.combinations(self.df_X.columns,2)
+            for col_comb in col_combs_inter:
+                self.df_X[str(col_comb[0]) + str(col_comb[1])] = self.df_X[col_comb[0]]*self.df_X[col_comb[1]]
+            self.df_X['ones'] = 1
 
-        self.X_decoded_FO = self.decodeUnits(self.X_first_order)
+        elif self.modelType == 'MIX':
+            self.df_X = df_X.copy(deep = True)
+            col_combs = itertools.combinations(self.df_X.columns,2)
+            for col_comb in col_combs:
+                self.df_X[str(col_comb[0]) + str(col_comb[1])] = self.df_X[col_comb[0]]*self.df_X[col_comb[1]]
+            self.df_X['X0X1X2'] = self.df_X['X0']*self.df_X['X1']*self.df_X['X2']
+
+        #self.X_decoded_FO = self.decodeUnits(self.X_first_order)
         #print(self.df_X_second_order)
         #print(self.decoded_FO_X)
-        return self.df_X_second_order,self.df_X_inter,self.df_X_first_order, self.X_second_order,self.X_inter,self.X_first_order, self.X_decoded_FO
+        self.X = self.df_X.values
+        return self.df_X, self.X
 
 
     def decodeUnits(self, X):
@@ -121,40 +125,44 @@ class RSM_regression(object):
         p_value_Lof = f.sf(F_Lof,m - p,self.N - m)
 
         # F-test for weights (compare full model to reduced model without each weight)
-        df_X_second_order_reduced =  self.df_X_second_order.iloc[:,:-2]
-        #W_reduced = W
-        reduced_F = []
-        reduced_p = []
-        SS_Sq_all = []
-        for i in range(p):
-            #holder = W[i] #remember the weight that is about to be set to 0
-            #W_reduced[i] = 0 #set the weight to 0
-            df_X_second_order_reduced = df_X_second_order_reduced.drop(df_X_second_order_reduced.columns[i], axis = 1)
+        if p > 2:
+            df_X_reduced =  self.df_X.iloc[:,:-2]
+            #W_reduced = W
+            reduced_F = []
+            reduced_p = []
+            SS_Sq_all = []
+            for i in range(p):
+                #holder = W[i] #remember the weight that is about to be set to 0
+                #W_reduced[i] = 0 #set the weight to 0
+                df_X_reduced = df_X_reduced.drop(df_X_reduced.columns[i], axis = 1)
 
-            W_reduced = self.weights(df_X_second_order_reduced,self.Y,l1,learning_rate,l2)
-            Yhat_reduced = self.forward(df_X_second_order_reduced, W_reduced)
+                W_reduced = self.weights(df_X_reduced,self.Y,l1,learning_rate,l2)
+                Yhat_reduced = self.forward(df_X_reduced, W_reduced)
 
-            #Yhat_reduced = self.forward(df_X.iloc[:,:-2], W_reduced) # predict on reduced model
-            #W_reduced[i] = holder
+                #Yhat_reduced = self.forward(df_X.iloc[:,:-2], W_reduced) # predict on reduced model
+                #W_reduced[i] = holder
 
-            SSRes_reduced = (self.Y - Yhat_reduced).dot(self.Y - Yhat_reduced)
-            SSReg_reduced = (Yhat_reduced - self.Y.mean()).dot(Yhat_reduced - self.Y.mean()) #Variation due to regression (how far the model is away from the no relationship situation (the mean))
+                SSRes_reduced = (self.Y - Yhat_reduced).dot(self.Y - Yhat_reduced)
+                SSReg_reduced = (Yhat_reduced - self.Y.mean()).dot(Yhat_reduced - self.Y.mean()) #Variation due to regression (how far the model is away from the no relationship situation (the mean))
 
-            MSRes_reduced = SSRes_reduced/(self.N - p - 1) # one of the weights is set to 0 so there is one less degrees of freedom
-            MSReg_reduced = SSReg_reduced/(p - 2) # one of the weights is set to 0 so there is one less degrees of freedom
+                MSRes_reduced = SSRes_reduced/(self.N - p - 1) # one of the weights is set to 0 so there is one less degrees of freedom
+                MSReg_reduced = SSReg_reduced/(p - 2) # one of the weights is set to 0 so there is one less degrees of freedom
 
-            #F_reduced = MSReg_reduced/MSRes
-            degFree_Res_reduced = degFree_Res + 1 #one less parameter
-            SS_Sq = SSRes_reduced - SSRes
-            F_reduced = ( ((SS_Sq)/(degFree_Res_reduced - degFree_Res))/(SSRes/degFree_Res) )
-            p_value_weight = f.sf(F_reduced, (degFree_Res_reduced - degFree_Res), degFree_Res)
-            reduced_F.append(F_reduced)
-            reduced_p.append(p_value_weight)
-            SS_Sq_all.append(SS_Sq)
+                #F_reduced = MSReg_reduced/MSRes
+                degFree_Res_reduced = degFree_Res + 1 #one less parameter
+                SS_Sq = SSRes_reduced - SSRes
+                F_reduced = ( ((SS_Sq)/(degFree_Res_reduced - degFree_Res))/(SSRes/degFree_Res) )
+                p_value_weight = f.sf(F_reduced, (degFree_Res_reduced - degFree_Res), degFree_Res)
+                reduced_F.append(F_reduced)
+                reduced_p.append(p_value_weight)
+                SS_Sq_all.append(SS_Sq)
 
-            df_X_second_order_reduced =  self.df_X_second_order.iloc[:,:-2]
+                df_X_reduced =  self.df_X.iloc[:,:-2]
 
-        df_results_weightStats = pd.DataFrame({'weight': df_X.iloc[:,:-2].columns, 'SS Sq': SS_Sq_all, 'F-statistic': reduced_F, 'p-value': reduced_p})
+        if p > 2:
+            df_results_weightStats = pd.DataFrame({'weight': df_X.iloc[:,:-2].columns, 'SS Sq': SS_Sq_all, 'F-statistic': reduced_F, 'p-value': reduced_p})
+        else:
+            df_results_weightStats = 'None'
         df_results = pd.DataFrame({'type':['Regression','Lack of fit'],'F-statistic': [F_reg,F_Lof], 'p-value': [p_value_reg, p_value_Lof], 'r-squared':[r2,'-'], 'SS':[SSReg,SSLof] })
         df_results.set_index('type',inplace = True)
 
@@ -167,10 +175,10 @@ class RSM_regression(object):
         return self.residuals
 
     def getWeights(self):
-        return dict(zip(self.df_X_second_order.columns, self.W_SO)), self.W_SO
+        return dict(zip(self.df_X.columns, self.W_SO)), self.W_SO
 
     def solveSecondOrder(self,X):
-        X = self.variable_engineer(X)[3]
+        X = self.variable_engineer(X)[1]
         return X.dot(self.W_SO)
 
     def solvedeCodedSO(self,X):
@@ -194,30 +202,19 @@ class RSM_regression(object):
         self.Y = Y
         #self.Y = np.reshape(self.Y,(Y.shape[0],1))
 
-        self.W_FO = self.weights(self.X_first_order,self.Y,l1,learning_rate,l2)
-        self.W_int = self.weights(self.X_inter,self.Y,l1,learning_rate,l2)
-        self.W_SO = self.weights(self.X_second_order,self.Y,l1,learning_rate,l2)
+        self.W = self.weights(self.X,self.Y,l1,learning_rate,l2)
 
-        self.Yhat_FO = self.forward(self.df_X_first_order,self.W_FO)
-        self.Yhat_int = self.forward(self.df_X_inter,self.W_int)
-        self.Yhat_SO = self.forward(self.df_X_second_order,self.W_SO)
+        self.Yhat = self.forward(self.df_X,self.W)
         #print(self.Yhat_FO)'''
 
-        #print('First order','\n',self.get_stats(self.df_X_first_order),'\n')
-        #print('Interactions', '\n',self.get_stats(self.df_X_inter),'\n')
-        #print('Second order', '\n',self.get_stats(self.df_X_second_order),'\n')
         print('==========================================================')
         print('                 Regression stats')
         print('==========================================================')
-        print('First order')
-        self.get_stats(self.df_X_first_order)
+        print('Model type: ', str(self.modelType))
+        self.get_stats(self.df_X)
         print('==========================================================')
-        print('Interactions')
-        self.get_stats(self.df_X_inter)
-        print('==========================================================')
-        print('Second order')
-        self.get_stats(self.df_X_second_order)
-        print('==========================================================')
+
+        return self.W
 
     def forwardPlot(self,X,W):
         if isinstance(X, pd.DataFrame):
@@ -225,35 +222,51 @@ class RSM_regression(object):
         return W[0]*((X[0] - 67.5)/25) + W[1]*((X[1] - 49)/14) + W[2]*((X[0] - 67.5)/25)**2 + W[3]*((X[0] - 67.5)/25)*((X[1] - 49)/14) \
          + W[4]*((X[1] - 49)/14)**2 + W[5]
 
-    def plot3D(self,model = 'SO', n_factor = 2, Max = [1.41, 1.41], Min = [-1.41,-1.41], a = 0, x_label = '', y_label = '', z_label = '', title = ''):
+    def plot3D(self, n_factor = 2, Max = [1.41, 1.41], Min = [-1.41,-1.41], a = 0, x_label = '', y_label = '', z_label = '', title = ''):
 
         fig = plt.figure()
         ax = fig.add_subplot(111,projection = '3d')
-        #ax.scatter(self.X_decoded_FO[:,0], self.X_decoded_FO[:,1], self.Y,color = 'r')
+        ax.scatter(self.X[:,0], self.X[:,1], self.Y,color = 'r')
 
-        x0 = np.linspace(Min[0],Max[0],50)
-        x1 = np.linspace(Min[1],Max[1],50)
-        XX0,XX1 = np.meshgrid(x0,x1)
+        if self.modelType == 'MIX':
+            x0 = np.linspace(0,1,50)
+            x1 = np.linspace(1,0,50)
+            XX0,XX1 = np.meshgrid(x0,x1)
 
-        if model == 'SO':
+        else:
+            x0 = np.linspace(Min[0],Max[0],50)
+            x1 = np.linspace(Min[1],Max[1],50)
+            XX0,XX1 = np.meshgrid(x0,x1)
+
+        if self.modelType == 'SO':
             if n_factor == 2:
-                ys = np.array([self.forwardPlot(np.array([X0,X1,X0*X0,X0*X1,X1*X1,1]),self.W_SO) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
+                ys = np.array([self.forwardPlot(np.array([X0,X1,X0*X0,X0*X1,X1*X1,1]),self.W) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
+                ys_colour_restraint = np.array([(-2.52926*((x - 49)/14) + 0.01168709*((x - 49)/14)**2 + 185.52329) for x in np.linspace(Min[1],Max[1],50)])
             if n_factor == 3:
-                ys = np.array([self.forwardPlot(np.array([X0,X1,a,X0*X0,X0*X1,X0*a,X1*X1,X1*a,a*a,1]),self.W_SO) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
-        elif model == 'int':
-            ys = np.array([self.forward(np.array([X0,X1,X0*X1,1]),self.W_int) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
-        elif model =='FO':
-            ys = np.array([self.forward(np.array([X0,X1,1]),self.W_FO) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
+                ys = np.array([self.forwardPlot(np.array([X0,X1,a,X0*X0,X0*X1,X0*a,X1*X1,X1*a,a*a,1]),self.W) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
+        elif self.modelType == 'INT':
+            ys = np.array([self.forward(np.array([X0,X1,X0*X1,1]),self.W) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
+        elif self.modelType =='FO':
+            ys = np.array([self.forward(np.array([X0,X1,1]),self.W) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
 
-        Y_plot = ys.reshape(XX1.shape)
+        elif self.modelType == 'MIX':
+            ys = np.array([self.forward(np.array([X0, X1, (1 - X0 -X1), X0*X1, X0*(1 - X0 -X1), X1*(1 - X0 - X1), X0*X1*(1 - X0 -X1)]),self.W) for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
 
-        ax.plot_surface(XX0,XX1,Y_plot, cmap = cm.autumn,alpha = 0.6)
+        Z = np.array([4.92561585 for X0,X1 in zip(np.ravel(XX0),np.ravel(XX1))])
+
+
+        ys = ys.reshape(XX1.shape)
+        if self.modelType == 'MIX':
+            ys[XX0 + XX1 >= 1] = np.nan
+
+        ax.plot_surface(XX0,XX1,ys, cmap = cm.winter,alpha = 0.6)
+        #ax.plot(x1,ys_colour_restraint, color = 'black')
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
         ax.set_zlabel(z_label)
         plt.title(title)
-        plt.show()
 
+        plt.show()
 
 
     def maxObjective_n2(self,X):
